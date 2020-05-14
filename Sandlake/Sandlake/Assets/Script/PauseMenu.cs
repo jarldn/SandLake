f8c778d5c7ea695c93cf894d8fb3c2b3a604215a
JesterVoltrex <JesterVoltrex@users.noreply.github.com>
2020-05-14 10:56:04 +0200

    Fixed

--- Sandlake/Sandlake/Assets/Script/PauseMenu.cs
+++ Sandlake/Sandlake/Assets/Script/PauseMenu.cs
@@ -5,8 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public void Exit ()
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Restart();
+            } else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Restart ()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu ()
+    {
+        SceneManager.LoadScene("Start Game");
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
     }
 }
