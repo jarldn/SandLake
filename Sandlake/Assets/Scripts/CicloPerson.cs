eab36cf84e3ade2549a53e296671fe352625b06f
Javi <javiroldi@hotmail.com>
2020-06-01 20:07:11 +0200

    actualización
    actualización

---
+++ Sandlake/Assets/Scripts/CicloPerson.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CicloPerson : MonoBehaviour
+{
+
+    float timer;
+    public float tiempoCiclo;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+     timer = 0;
+     animator = GetComponent<Animator>();
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer>tiempoCiclo)
+        {
+            animator.SetTrigger("Anim");
+            timer = 0;
+        }
+        
+
+        
+    }
+}
